447965b2642673b534c961a046e4f3460a1aafa3
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-23 19:16:18 +0000

    Add zeynep to index.js

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,8 @@ const User = require('./user')
 
 const armagan = new User('armagan', 20)
 const steve = new User('steve', 22)
+const zeynep = new User('zeynep', 38)
 
-const users = [armagan, steve]
+const users = [armagan, steve, zeynep]
 
 module.exports = { users }
