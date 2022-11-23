0b2dd73b870b1d5cdad5cc21c113118334a6c383
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-23 16:12:24 +0000

    Change title in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: `Coyotiv's Express starter Template` })
+  res.render('index', { title: `BE103` })
 })
 
 module.exports = router
