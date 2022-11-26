397593cf125ebcbf3b0b1b21c37dc01e9a36a204
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-26 11:31:51 +0000

    Change user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,37 @@
+const Review = require('./review')
+
 class User {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+    this.likes = []
+    this.basket = []
+    this.reviews = []
+  }
+
+  greet() {
+    console.log(`Hello ${this.name}, welcome to Joy Toys!`)
+  }
+
+  likeProduct(product) {
+    this.likes.push(product)
+    product.likedBy.push(this)
+  }
+
+  addToBasket(product) {
+    this.basket.push(product)
+  }
+
+  makeReview(product, text, rate) {
+    const review = new Review(text, rate, this)
+    this.reviews.push(review)
+    product.reviews.push(review)
+  }
+}
+
+module.exports = User
+
+/* class User {
   constructor(name, age) {
     this.name = name
     this.age = age
@@ -10,4 +43,4 @@ class User {
   }
 }
 
-module.exports = User
+module.exports = User */
