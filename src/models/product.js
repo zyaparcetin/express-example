836c1cc654c3cce363c7470d1fcb4111f95f00f3
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-26 13:10:18 +0000

    Add get detail

--- src/models/product.js
+++ src/models/product.js
@@ -5,6 +5,14 @@ class Product {
     this.likedBy = []
     this.reviews = []
   }
+
+  get detail() {
+    return `
+      Product: ${this.name} , ${this.price}
+      Liked by: ${this.likedBy}
+      Reviewed by: ${this.reviewedBy}
+      `
+  }
 }
 
-module.exports = Product
+// module.exports = Product
