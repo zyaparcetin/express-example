4b393d2736d6624fa7c5c01477c5aa1dc6a4f6ff
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-26 11:31:01 +0000

    Change index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,22 @@
 const User = require('./user')
+const Product = require('./product')
+// const Review = require('./review')
+
+const kerem = new User('kerem', 11)
+const mete = new User('mete', 7)
+
+const teddyBear = new Product('teddy bear', 19.99)
+const kitty = new Product('kitty', 14.99)
+
+kerem.addToBasket(kitty)
+mete.addToBasket(teddyBear)
+
+console.log(kerem)
+console.log(mete)
+
+// module.exports = { users }
+
+/* const User = require('./user')
 
 const armagan = new User('armagan', 20)
 const steve = new User('steve', 22)
@@ -6,4 +24,4 @@ const zeynep = new User('zeynep', 38)
 
 const users = [armagan, steve, zeynep]
 
-module.exports = { users }
+module.exports = { users } */
