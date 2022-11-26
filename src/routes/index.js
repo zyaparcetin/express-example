60b1cdbda038f5ab17196fec97ece181cdf9a767
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-26 20:57:17 +0000

    Commet out the lines

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
-/* const express = require('express')
+const express = require('express')
 
 const router = express.Router()
 
 /* GET home page. */
-/* router.get('/', (req, res) => {
-  res.render('index', { title: `Zeynep's Project : Joy Toys })
+router.get('/', (req, res) => {
+  res.render('index', { title: `Zeynep's Project : Joy Toys` })
 })
 
-module.exports = router */
+module.exports = router
