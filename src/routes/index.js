36d8a56112b64db071154d3c3a734483a3023d43
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-26 11:33:01 +0000

    Comment out index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
-const express = require('express')
+/* const express = require('express')
 
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res) => {
+/* router.get('/', (req, res) => {
   res.render('index', { title: `FE102` })
 })
 
-module.exports = router
+module.exports = router */
