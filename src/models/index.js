e404cca855c173835eff83d692e61da782f740d6
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-26 20:56:26 +0000

    Add some fuctions and module.exports

--- src/models/index.js
+++ src/models/index.js
@@ -11,13 +11,21 @@ const bunny = new Product('bunny', 9.99)
 const froglet = new Product('froglet', 9.99)
 const products = [teddyBear, kitty, bunny, froglet]
 
+mete.greet()
+
 kerem.addToBasket(kitty)
 mete.addToBasket(teddyBear)
+
+kerem.likeProduct(kitty)
+
+kerem.review(kitty, 'Wonderful toy!', 5)
 
 console.log(kerem)
 console.log(mete)
 
-// module.exports = { users }   bunu yazmali miyim?
+console.log(kitty)
+
+module.exports = { products }
 
 /* const User = require('./user')
 
